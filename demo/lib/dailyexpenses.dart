157ca2a95e45f511894465b7c2e56491d8be2355
Amirul Asraf <amirul.asraf9135@gmail.com>
2023-11-05 09:30:38 +0800

    comments

--- demo/lib/dailyexpenses.dart
+++ demo/lib/dailyexpenses.dart
@@ -41,6 +41,7 @@ class _ExpenseListState extends State<ExpenseList> {
           path: "/api/Time/current/zone?timeZone=Asia/Kuala_Lumpur",
           server: "https://timeapi.io");
       await req.get();
+      // only proceed if status code is 200 == success
       if (req.status() == 200) {
         dynamic res = req.result();
         String dateTime =
@@ -128,6 +129,7 @@ class _ExpenseListState extends State<ExpenseList> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    //moved message to init()
 
     return Scaffold(
       appBar: AppBar(
@@ -282,8 +284,10 @@ class Expense {
       : desc = json['desc'] as String,
         amount = (json['amount'] as dynamic)
             .toDouble(), //sometime dart assume non decimal number as int and can't cast it to double auto. so take it as dynamic and cast to double
+            // json string value 10 will be treated as int 10 and can't be assigned into double
         dateTime = json['dateTime'] as String;
 
+  // toJson will be automatically called by jsonEncode when necessary
   Map<String, dynamic> toJson() =>
       {'desc': desc, 'amount': amount, 'dateTime': dateTime};
 
--- demo/lib/request_controller.dart
+++ demo/lib/request_controller.dart
@@ -1,18 +1,24 @@
-import 'dart:convert';
+import 'dart:convert'; // json encode/decode
 import 'package:http/http.dart' as http;
 
 class RequestController { 
 
   String path;
   String server;
-  http.Response? _res;
+  http.Response? _res;// nullable response object (intially null until request is sent via post/get)
 
   final Map<dynamic, dynamic> _body = {};
   final Map<String,String> _headers= {};
   dynamic _resultData;
 
   RequestController({required this.path, 
-  this.server = "http://10.0.2.2" // default value to point to own server address unless other 3rd party address specified
+  // default value to point to own server address unless other 3rd party address specified
+  this.server = "http://192.168.8.102"
+ // this.server = "http://10.0.2.2" 
+ // 10.0.2.2 is default ip address for EMULATED to device to point to the local device which runs the emulator
+ // if your local device is connected to same network as server device use the server ip address
+ // Emulated device by default share same network with the device it runs on
+ // thus actual server ip address can also be used in emulated envorenment
   });
 
   setBody(Map<String, dynamic> data) {
@@ -38,10 +44,12 @@ class RequestController {
   }
 
   void _parseResult(){
+    // parse result into json structure if possible
     try{
       print("raw response:${_res?.body}" );
       _resultData = jsonDecode(_res?.body?? ""); 
     }catch(ex){
+      // otherwise the response body will be stored as is
       _resultData = _res?.body;
       print("exception in http result parsing ${ex}");
     }
@@ -53,6 +61,6 @@ class RequestController {
  
 
   int status() {
-    return _res?.statusCode ?? 0;
+    return _res?.statusCode ?? 0; //return the http response code, if no request sent or response yet, 0 will be returned
   }
 }
